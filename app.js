aeaf33d3e17083ab833435d85e26d6a937c8d038
Scott Milliorn <scottmilliorn@gmail.com>
2022-12-17 23:18:17 -0800

    Update app.js
    Signed-off-by: Scott Milliorn <scottmilliorn@gmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 require("dotenv").config();
 //console.log(process.env) // remove this after you've confirmed it is working
 
-const test = process.env;
-
 const express = require("express");
 const path = require("path");
+
 // https://expressjs.com/en/5x/api.html#app.use
 const app = express();
 const port = process.env.PORT || 4000;
